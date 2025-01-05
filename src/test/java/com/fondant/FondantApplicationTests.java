c2238ea2146b44f049f6baae3e60560a493f9d76
onegqueen <rkdwldnjs30@naver.com>
2025-01-05 20:39:36 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/com/fondant/FondantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fondant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
