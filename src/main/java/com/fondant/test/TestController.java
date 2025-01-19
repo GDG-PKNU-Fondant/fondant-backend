7a63dd44872db29881a54e3be607d7c376853fbf
onegqueen <rkdwldnjs30@naver.com>
2025-01-19 19:48:21 +0900

    docs: global dto 사용법 추가 ref:#5

---
+++ src/main/java/com/fondant/test/TestController.java
@@ -0,0 +1,20 @@
+package com.fondant.test;
+
+import com.fondant.global.dto.ResponseDto;
+import com.fondant.global.dto.SuccessMessage;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/success")
+    public ResponseEntity<ResponseDto<TestDto>> successTest(){
+        return ResponseEntity.ok(ResponseDto.ofSuccess(SuccessMessage.OPERATION_SUCCESS,
+                TestDto.of("강지원",25)));
+    }
+}
