cc5afee511131e06e9bf58922fd0165556ba97a2
pu2rile <3o920@naver.com>
2025-02-04 19:10:53 +0900

    feat: 카테고리별 마켓 조회 서비스 로직 추가
    -ref:#14

---
+++ src/main/java/com/fondant/market/application/MarketService.java
@@ -0,0 +1,54 @@
+package com.fondant.market.application;
+
+import com.fondant.global.dto.PageInfo;
+import com.fondant.market.application.dto.MarketInfo;
+import com.fondant.market.domain.entity.MarketEntity;
+import com.fondant.market.domain.repository.MarketRepository;
+import com.fondant.market.presentation.dto.response.MarketsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MarketService {
+    private final MarketRepository marketRepository;
+
+    @Autowired
+    public MarketService(MarketRepository marketRepository) {
+        this.marketRepository = marketRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public MarketsResponse getMarketsByCategoryId(Long categoryId, Pageable pageable) {
+        Page<MarketEntity> markets = marketRepository.findMarketsByCategory(categoryId,pageable);
+
+        return MarketsResponse.builder()
+                .pageInfo(PageInfo.of(markets.getNumber(), markets.getTotalPages()))
+                .markets(getMarketInfos(markets.getContent()))
+                .build();
+    }
+
+    private List<MarketInfo> getMarketInfos(List<MarketEntity> markets) {
+        return markets.stream()
+                .map(market -> MarketInfo.builder()
+                        .id(market.getId())
+                        .name(market.getName())
+                        .description(market.getDescription())
+                        .thumbnail(market.getThumbnail())
+                        .build())
+                .toList();
+    }
+
+    private MarketInfo convertToDto(MarketEntity market) {
+        return MarketInfo.builder()
+                .id(market.getId())
+                .name(market.getName())
+                .description(market.getDescription())
+                .thumbnail(market.getThumbnail())
+                .build();
+    }
+}
