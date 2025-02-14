65aefb2c96c551f196f901b4e6472a371a1adb24
pu2rile <3o920@naver.com>
2025-02-14 19:22:22 +0900

    refactor: 카테고리별 마켓 조회 api 경로 수정
    - ref:#14

--- src/main/java/com/fondant/market/presentation/MarketController.java
+++ src/main/java/com/fondant/market/presentation/MarketController.java
@@ -5,22 +5,25 @@ import com.fondant.global.dto.ResponseDto;
 import com.fondant.global.dto.SuccessMessage;
 import com.fondant.market.application.MarketService;
 import com.fondant.market.presentation.dto.response.MarketsResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/market")
+@RequiredArgsConstructor
+@RequestMapping("/api/markets")
 public class MarketController {
 
     private final MarketService marketService;
     private final PageConfig pageConfig;
 
-    public MarketController(MarketService marketService) {
-        this.marketService = marketService;
+    @GetMapping("/{marketId}")
+    public ResponseEntity<Object> getMarket(@PathVariable long marketId) {
+        return ResponseEntity.ok(null);
     }
 
-    @GetMapping("/{categoryId}")
+    @GetMapping("/categories/{categoryId}")
     public ResponseEntity<ResponseDto<MarketsResponse>> getMarketsByCategoryId(
             @PathVariable(name = "categoryId") Long categoryId,
             @RequestParam(required = false) Pageable pageable) {
