ad2afaa9ff2fd6c2d55f494d8ba3e8564628ef8b
이상윤 <leesy010504@gmail.com>
2025-02-19 03:56:14 +0900

    feat: CurrentUser Custom Annotation 추가
    - ref: #20

---
+++ src/main/java/com/fondant/global/annotation/CurrentUser.java
@@ -0,0 +1,16 @@
+package com.fondant.global.annotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Bean
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal
+public @interface CurrentUser {
+}
