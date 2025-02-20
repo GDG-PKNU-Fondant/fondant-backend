b1a17a0587f6368e6540a2397765ea381a75d8d6
pu2rile <3o920@naver.com>
2025-02-20 11:58:01 +0900

    feat: 마켓 상세 정보 dto 추가
    -ref:#14

---
+++ src/main/java/com/fondant/market/application/dto/MarketDetail.java
@@ -0,0 +1,29 @@
+package com.fondant.market.application.dto;
+
+import com.fondant.market.domain.entity.MarketEntity;
+import lombok.Builder;
+
+@Builder
+public record MarketDetail(
+        Long id,
+        String name,
+        String description,
+        String thumbnail,
+        String background,
+        Long totalSales,
+        Long totalReviews,
+        Long deliveryFee
+) {
+    public static MarketDetail of(MarketEntity market) {
+        return MarketDetail.builder()
+                .id(market.getId())
+                .name(market.getName())
+                .description(market.getDescription())
+                .thumbnail(market.getThumbnail())
+                .background(market.getBackground())
+                .totalSales(market.getTotalSales())
+                .totalReviews(market.getTotalReviews())
+                .deliveryFee(market.getDeliveryFee())
+                .build();
+    }
+}
