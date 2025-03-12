720eb05de54f2f3b66dac39cc52f7025ce85cdc9
pu2rile <3o920@naver.com>
2025-03-12 21:04:50 +0900

    docs, test: 카테고리별 인기 Top30 restdocs, 테스트 추가
    -mock데이터 수정
    -ref:#27

--- src/test/java/com/fondant/restdocs/MarketRestDocsTest.java
+++ src/test/java/com/fondant/restdocs/MarketRestDocsTest.java
@@ -17,9 +17,6 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
-import static com.fondant.restdocs.ProductRestDocsTest.commonResponseFields;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -49,7 +46,9 @@ public class MarketRestDocsTest {
 
     private MarketEntity market1;
     private MarketEntity market2;
-    private CategoryEntity category;
+    private MarketEntity market3;
+    private CategoryEntity category1;
+    private CategoryEntity category2;
 
     private static final String BASE_URL = "/api/markets";
 
@@ -59,36 +58,54 @@ public class MarketRestDocsTest {
         categoryRepository.deleteAll();
         marketRepository.deleteAll();
         
-        category = categoryRepository.save(CategoryEntity.builder()
+        category1 = categoryRepository.save(CategoryEntity.builder()
                 .name("쿠키")
                 .build());
 
+        category2 = categoryRepository.save(CategoryEntity.builder()
+                .name("빵")
+                .build());
+
         market1 = marketRepository.save(MarketEntity.builder()
-                .name("Market A")
-                .description("쿠키를 판매하는 마켓 A")
+                .name("상윤이네 쿠키")
+                .description("쿠키를 판매하는 마켓")
                 .thumbnail("market-a-thumbnail.jpg")
                 .background("market-a-bg.jpg")
-                .totalSales(50L)
+                .totalSales(500L)
                 .totalReviews(100L)
                 .build());
 
         market2 = marketRepository.save(MarketEntity.builder()
-                .name("Market B")
-                .description("쿠키를 판매하는 마켓 B")
+                .name("명인 강지원")
+                .description("쿠키를 판매하는 마켓")
                 .thumbnail("market-b-thumbnail.jpg")
                 .background("market-b-bg.jpg")
-                .totalSales(50L)
-                .totalReviews(2000L)
+                .totalSales(500L)
+                .totalReviews(200L)
+                .build());
+
+        market3 = marketRepository.save(MarketEntity.builder()
+                .name("도현베이커리")
+                .description("빵을 판매하는 마켓")
+                .thumbnail("market-b-thumbnail.jpg")
+                .background("market-b-bg.jpg")
+                .totalSales(100L)
+                .totalReviews(70L)
                 .build());
 
         marketCategoryRepository.save(MarketCategoryEntity.builder()
                 .market(market1)
-                .category(category)
+                .category(category1)
                 .build());
 
         marketCategoryRepository.save(MarketCategoryEntity.builder()
                 .market(market2)
-                .category(category)
+                .category(category1)
+                .build());
+
+        marketCategoryRepository.save(MarketCategoryEntity.builder()
+                .market(market3)
+                .category(category2)
                 .build());
     }
     public static FieldDescriptor[] commonResponseFields() {
@@ -101,7 +118,7 @@ public class MarketRestDocsTest {
 
     @Test
     void getMarketsByCategory() throws Exception {
-        mockMvc.perform(get(BASE_URL + "/categories/{categoryId}", category.getId())
+        mockMvc.perform(get(BASE_URL + "/categories/{categoryId}", category2.getId())
                         .param("page", "0")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -160,6 +177,37 @@ public class MarketRestDocsTest {
                 .andDo(document("markets/get-top10-markets-by-popularity",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
+                        queryParameters(
+                                parameterWithName("page").description("페이지 번호 (0부터 시작)")
+                        ),
+                        responseFields(
+                                commonResponseFields()
+                        ).andWithPrefix("response.", new FieldDescriptor[]{
+                                fieldWithPath("pageInfo").description("페이지 정보"),
+                                fieldWithPath("pageInfo.currentPage").description("현재 페이지"),
+                                fieldWithPath("pageInfo.totalPage").description("전체 페이지"),
+                                fieldWithPath("markets").description("마켓 목록"),
+                                fieldWithPath("markets[].id").description("마켓 ID"),
+                                fieldWithPath("markets[].name").description("마켓 이름"),
+                                fieldWithPath("markets[].description").description("마켓 한줄 소개"),
+                                fieldWithPath("markets[].thumbnail").description("마켓 썸네일 이미지 URL"),
+                                fieldWithPath("markets[].totalSales").description("총 판매 수량"),
+                                fieldWithPath("markets[].totalReviews").description("총 리뷰 개수")
+                        })));
+    }
+
+    @Test
+    void getTop30MarketsByCategory() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/{categoryId}/top30", category1.getId())
+                .param("page", "0")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("markets/get-top30-markets-by-category",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("categoryId").description("조회할 카테고리 ID")
+                        ),
                         queryParameters(
                                 parameterWithName("page").description("페이지 번호 (0부터 시작)")
                         ),
