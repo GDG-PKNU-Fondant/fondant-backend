2c88e3da12f92e25056f5f1177b9a964737613b8
pu2rile <3o920@naver.com>
2025-03-20 22:00:54 +0900

    feat: 마켓 상세 조회 dto 내부 수정
    -isLiked, likeCount, isTop10, hashtags 필드 추가
    -ref:#31

--- src/main/java/com/fondant/market/application/dto/MarketDetail.java
+++ src/main/java/com/fondant/market/application/dto/MarketDetail.java
@@ -3,21 +3,37 @@ package com.fondant.market.application.dto;
 import com.fondant.market.domain.entity.MarketEntity;
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
 public record MarketDetail(
         Long id,
         String name,
         String description,
         String thumbnail,
-        String background
+        String background,
+        boolean liked,
+        Long likeCount,
+        boolean isTop10,
+        List<String> hashtags
 ) {
-    public static MarketDetail of(MarketEntity market) {
-        return MarketDetail.builder()
-                .id(market.getId())
-                .name(market.getName())
-                .description(market.getDescription())
-                .thumbnail(market.getThumbnail())
-                .background(market.getBackground())
-                .build();
+    public static MarketDetail of(
+            MarketEntity market,
+            boolean liked,
+            Long likeCount,
+            boolean isTop10,
+            List<String> hashtags
+    ){
+        return new MarketDetail(
+                market.getId(),
+                market.getName(),
+                market.getDescription(),
+                market.getThumbnail(),
+                market.getBackground(),
+                liked,
+                likeCount != null ? likeCount : 0L,
+                isTop10,
+                hashtags
+        );
     }
 }
