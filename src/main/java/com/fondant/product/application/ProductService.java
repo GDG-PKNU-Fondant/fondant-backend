810b49d0cb71e1cecea46823732f2e61d6ae7446
onegqueen <rkdwldnjs30@naver.com>
2025-04-25 00:47:54 +0900

    feat: 마켓정보 dto 생성및 응답에 추가 ref:#58
    # Conflicts:
    #	src/main/java/com/fondant/product/application/ProductService.java

--- src/main/java/com/fondant/product/application/ProductService.java
+++ src/main/java/com/fondant/product/application/ProductService.java
@@ -4,6 +4,7 @@ import com.fondant.global.dto.PageInfo;
 import com.fondant.global.exception.ApiException;
 import com.fondant.market.application.MarketService;
 import com.fondant.market.application.dto.MarketInfo;
+import com.fondant.market.application.dto.MarketInfoForProductDetail;
 import com.fondant.market.domain.entity.MarketEntity;
 import com.fondant.market.domain.repository.MarketRepository;
 import com.fondant.product.application.dto.ImageInfo;
@@ -78,18 +79,32 @@ public class ProductService {
         return (int) Math.floor(price * appliedRate + 0.5);
     }
 
+    @Transactional(readOnly = true)
     public ProductDetailResponse getProductDetail(Long productId) {
         ProductEntity product = getProductById(productId);
+
         return ProductDetailResponse.builder()
                 .photos(getImageUrlsByProductIdAndType(productId,ImageType.PRODUCT_PHOTO))
                 .name(product.getName())
                 .options(getOptionInfos(productId))
                 .description(product.getDescription())
                 .detailPages(getImageUrlsByProductIdAndType(productId,ImageType.DETAIL_PAGE))
+                .marketInfo(getMarketInfo(product.getMarket()))
                 .basePrice(product.getPrice())
                 .build();
     }
 
+    private MarketInfoForProductDetail getMarketInfo(MarketEntity market) {
+        return MarketInfoForProductDetail.builder()
+                .id(market.getId())
+                .name(market.getName())
+                .description(market.getDescription())
+                .thumbnail(market.getThumbnail())
+                .totalReviews(market.getTotalReviews())
+                .totalSales(market.getTotalSales())
+                .freeDeliveryLimit(market.getFreeDeliveryLimit())
+                .build();
+    }
     private List<ImageInfo> getImageUrlsByProductIdAndType(Long productId, ImageType imageType){
         return productImageRepository.findByProductIdAndImageTypeOrderByImgOrderAsc(productId, imageType)
                 .stream().map(img->
