0f7b6d5f80a756f6297e4f43c5e000aa209b89e1
pu2rile <3o920@naver.com>
2025-05-21 15:41:43 +0900

    refactor: 장바구니 응답을 마켓 기준으로 변경
    -ref:#59

--- src/main/java/com/fondant/cart/presentation/dto/response/CartResponse.java
+++ src/main/java/com/fondant/cart/presentation/dto/response/CartResponse.java
@@ -1,6 +1,6 @@
 package com.fondant.cart.presentation.dto.response;
 
-import com.fondant.cart.application.dto.CartInfo;
+import com.fondant.cart.application.dto.CartMarketInfo;
 import com.fondant.cart.domain.entity.CartItemEntity;
 import com.fondant.global.dto.PageInfo;
 import lombok.Builder;
@@ -10,12 +10,12 @@ import java.util.List;
 @Builder
 public record CartResponse(
         PageInfo pageInfo,
-        List<CartInfo> cartItems
+        List<CartMarketInfo> markets
 ) {
-    public static CartResponse of(List<CartItemEntity> cartItems, PageInfo pageInfo) {
+    public static CartResponse of(List<CartMarketInfo> marketInfos, PageInfo pageInfo) {
         return CartResponse.builder()
+                .markets(marketInfos)
                 .pageInfo(pageInfo)
-                .cartItems(cartItems.stream().map(CartInfo::from).toList())
                 .build();
     }
 }
