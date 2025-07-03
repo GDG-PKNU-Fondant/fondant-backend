b6e82bebeb0ebc964d087b9a0bd70bb974636a97
pu2rile <3o920@naver.com>
2025-07-03 19:13:23 +0900

    feat: 장바구니 엔티티 getter 추가
    -ref: #71

--- src/main/java/com/fondant/cart/domain/entity/CartEntity.java
+++ src/main/java/com/fondant/cart/domain/entity/CartEntity.java
@@ -4,10 +4,12 @@ import com.fondant.user.domain.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CartEntity {
