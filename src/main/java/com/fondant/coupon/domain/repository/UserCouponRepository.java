cb5b11bd92dc8f4d199334b23fafbce62528b4f1
이상윤 <leesy010504@gmail.com>
2025-07-17 05:05:10 +0900

    feat: Coupon repository 추가
    - ref: #93

--- src/main/java/com/fondant/coupon/domain/repository/UserCouponRepository.java
+++ src/main/java/com/fondant/coupon/domain/repository/UserCouponRepository.java
@@ -4,7 +4,11 @@ import com.fondant.coupon.domain.entity.UserCouponEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
-public interface UserCouponRepository extends JpaRepository<UserCouponEntity, Long>{
+public interface UserCouponRepository extends JpaRepository<UserCouponEntity, Long>, UserCouponRepositoryCustom {
     boolean existsByUserIdAndCouponId(Long userId, Long couponId);
+    List<UserCouponEntity> findValidUserCoupons(Long userId, Set<Long> couponIds);
 }
